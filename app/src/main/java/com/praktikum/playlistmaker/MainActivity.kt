8c3c9477a8670911c5c0327446203a9e3702e62f
Xenia Dorofeeva <xenia.edu.projects@gmail.com>
2026-02-21 18:23:38 +0100

    Fix warning from the idea, do not use layout in the main activity - not needed now

--- app/src/main/java/com/praktikum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/praktikum/playlistmaker/MainActivity.kt
@@ -2,13 +2,10 @@ package com.praktikum.playlistmaker
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import com.praktikum.playlistmaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.activity_main)
     }
 }
