3f496ea7e563ff5619a59321c9d991b85063b0b2
Xenia Dorofeeva <xenia.edu.projects@gmail.com>
2026-02-21 18:04:42 +0100

    Add the settings screen using a layout component and databinding lib

--- app/src/main/java/com/praktikum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/praktikum/playlistmaker/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.praktikum.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.praktikum.playlistmaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
