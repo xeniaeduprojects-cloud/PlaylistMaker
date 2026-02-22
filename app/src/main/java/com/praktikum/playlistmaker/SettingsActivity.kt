fd77b5de77b2db4fab9233cb79492f78710add3e
Xenia Dorofeeva <xenia.edu.projects@gmail.com>
2026-02-22 10:19:33 +0100

    Fix the icons color in the settings: should be grey

---
+++ app/src/main/java/com/praktikum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package com.praktikum.playlistmaker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
