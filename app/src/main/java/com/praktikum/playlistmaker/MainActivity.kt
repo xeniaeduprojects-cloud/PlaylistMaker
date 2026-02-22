a5a61e43e27c8460e2a36beb3380843dcdd58eb0
Xenia Dorofeeva <xenia.edu.projects@gmail.com>
2026-02-22 10:54:31 +0100

    Add search, media library activities and simple nvagation from the main menu

--- app/src/main/java/com/praktikum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/praktikum/playlistmaker/MainActivity.kt
@@ -1,32 +1,45 @@
 package com.praktikum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         val btnSearch = findViewById<Button>(R.id.btnSearch)
-        btnSearch.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Search button", Toast.LENGTH_SHORT).show()
+        val btnSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val displayIntent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(displayIntent)
+            }
         }
+        btnSearch.setOnClickListener(btnSearchClickListener)
+
 
         val btnMediaLibrary = findViewById<Button>(R.id.btnMediaLibrary)
         btnMediaLibrary.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Media library button", Toast.LENGTH_SHORT).show()
+            val displayIntent = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(displayIntent)
         }
 
         val btnSettings = findViewById<Button>(R.id.btnSettings)
-        val btnSettingsClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Settings button", Toast.LENGTH_SHORT).show()
-            }
+        btnSettings.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
         }
-        btnSettings.setOnClickListener(btnSettingsClickListener)
     }
 }
--- app/src/main/java/com/praktikum/playlistmaker/MediaLibraryActivity.kt
+++ app/src/main/java/com/praktikum/playlistmaker/MediaLibraryActivity.kt
@@ -5,14 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.praktikum.playlistmaker.databinding.ActivitySettingsBinding
 
-class SettingsActivity : AppCompatActivity() {
+class MediaLibraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val binding = ActivitySettingsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_media_library)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
