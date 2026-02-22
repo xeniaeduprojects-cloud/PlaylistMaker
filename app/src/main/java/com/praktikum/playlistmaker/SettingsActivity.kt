29b1552e31ca35fabc3ce9613cd002b4d9a7cba7
Xenia Dorofeeva <xenia.edu.projects@gmail.com>
2026-02-22 11:00:40 +0100

    Use separate Image View instead of compound drawable and add listener to it

--- app/src/main/java/com/praktikum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/praktikum/playlistmaker/SettingsActivity.kt
@@ -1,5 +1,6 @@
 package com.praktikum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +18,12 @@ class SettingsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnReturnFromSettings = binding.btnReturnFromSettings
+        btnReturnFromSettings.setOnClickListener {
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
         }
     }
 }
