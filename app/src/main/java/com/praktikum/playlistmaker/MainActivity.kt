7e0f4831339e2635dc6edac8aea7343fa2e0b5e7
Xenia Dorofeeva <xenia.edu.projects@gmail.com>
2026-02-22 09:50:15 +0100

    Add button listeners to the main screen

--- app/src/main/java/com/praktikum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/praktikum/playlistmaker/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.praktikum.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSearch = findViewById<Button>(R.id.btnSearch)
+        btnSearch.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Search button", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnMediaLibrary = findViewById<Button>(R.id.btnMediaLibrary)
+        btnMediaLibrary.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Media library button", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnSettings = findViewById<Button>(R.id.btnSettings)
+        val btnSettingsClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Settings button", Toast.LENGTH_SHORT).show()
+            }
+        }
+        btnSettings.setOnClickListener(btnSettingsClickListener)
     }
 }
