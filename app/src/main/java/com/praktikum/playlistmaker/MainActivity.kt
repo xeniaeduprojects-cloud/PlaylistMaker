c9181bc45cf5bea32fc65b9f29b4bbf0a029f60e
Xenia Dorofeeva <xenia.edu.projects@gmail.com>
2026-02-23 20:13:32 +0100

    ReviewFix: get rid of inner LinearLayouts

--- app/src/main/java/com/praktikum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/praktikum/playlistmaker/MainActivity.kt
@@ -3,7 +3,7 @@ package com.praktikum.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import com.google.android.material.button.MaterialButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val btnSearch = findViewById<Button>(R.id.btnSearch)
+        val btnSearch = findViewById<MaterialButton>(R.id.btnSearch)
         val btnSearchClickListener: View.OnClickListener = object : View.OnClickListener {
             override fun onClick(v: View?) {
                 val displayIntent = Intent(this@MainActivity, SearchActivity::class.java)
@@ -30,13 +30,13 @@ class MainActivity : AppCompatActivity() {
         btnSearch.setOnClickListener(btnSearchClickListener)
 
 
-        val btnMediaLibrary = findViewById<Button>(R.id.btnMediaLibrary)
+        val btnMediaLibrary = findViewById<MaterialButton>(R.id.btnMediaLibrary)
         btnMediaLibrary.setOnClickListener {
             val displayIntent = Intent(this, MediaLibraryActivity::class.java)
             startActivity(displayIntent)
         }
 
-        val btnSettings = findViewById<Button>(R.id.btnSettings)
+        val btnSettings = findViewById<MaterialButton>(R.id.btnSettings)
         btnSettings.setOnClickListener {
             val displayIntent = Intent(this, SettingsActivity::class.java)
             startActivity(displayIntent)
