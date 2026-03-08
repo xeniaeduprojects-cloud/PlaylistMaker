9af7dce4d4e4a87f0be43db5f12f49b20018e535
Xenia Dorofeeva <xenia.edu.projects@gmail.com>
2026-03-08 19:07:48 +0100

    Implement dark mode toggle

--- app/src/main/java/com/praktikum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/praktikum/playlistmaker/SettingsActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import android.content.res.Configuration
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.praktikum.playlistmaker.databinding.ActivitySettingsBinding
@@ -25,6 +27,16 @@ class SettingsActivity : AppCompatActivity() {
             finish()
         }
 
+        binding.switchDarkTheme.isChecked =
+            (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+
+        binding.switchDarkTheme.setOnCheckedChangeListener { _, isChecked ->
+            AppCompatDelegate.setDefaultNightMode(
+                if (isChecked) AppCompatDelegate.MODE_NIGHT_YES
+                else AppCompatDelegate.MODE_NIGHT_NO
+            )
+        }
+
         binding.textViewShare.setOnClickListener {
             val message = getString(R.string.share_message)
             val intent = Intent(Intent.ACTION_SEND).apply {
@@ -33,7 +45,7 @@ class SettingsActivity : AppCompatActivity() {
             }
 
             val title = getString(R.string.share_chooser_title)
-            startActivity(Intent.createChooser(intent, title));
+            startActivity(Intent.createChooser(intent, title))
         }
 
         binding.textViewSupport.setOnClickListener {
