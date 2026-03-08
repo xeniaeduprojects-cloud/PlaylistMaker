7115295392c9474974720a5c0d34063ea48c3444
Xenia Dorofeeva <xenia.edu.projects@gmail.com>
2026-03-08 14:09:58 +0100

    Add a search bar

--- app/src/main/java/com/praktikum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/praktikum/playlistmaker/SearchActivity.kt
@@ -1,9 +1,13 @@
 package com.praktikum.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import com.praktikum.playlistmaker.databinding.ActivitySearchBinding
 
@@ -21,6 +25,21 @@ class SearchActivity : AppCompatActivity() {
 
         binding.toolbarSearch.setNavigationOnClickListener {
             finish()
+        }
+
+        binding.searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.searchClearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        binding.searchClearButton.setOnClickListener {
+            binding.searchEditText.text.clear()
+
+            WindowCompat.getInsetsController(window, binding.searchEditText)
+                .hide(WindowInsetsCompat.Type.ime())
         }
     }
 }
