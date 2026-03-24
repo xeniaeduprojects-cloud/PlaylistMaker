3f13d011941b8403793ead2048a25eb5905922c2
Xenia Dorofeeva <xenia.edu.projects@gmail.com>
2026-03-24 08:48:23 +0100

    Add Track data class and TrackRepository interface

---
+++ app/src/main/java/com/praktikum/playlistmaker/search/data/model/Track.kt
@@ -0,0 +1,8 @@
+package com.praktikum.playlistmaker.search.data.model
+
+data class Track(
+    val trackName: String,
+    val artistName: String,
+    val trackTime: String,
+    val artworkUrl100: String,
+)
