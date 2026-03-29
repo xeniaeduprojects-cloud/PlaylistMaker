bc0cc16e04344cdf6b55a145f74e5bf92a5a82ab
Xenia Dorofeeva <xenia.edu.projects@gmail.com>
2026-03-29 15:40:47 +0200

    Go through the checklist

--- app/src/main/java/com/praktikum/playlistmaker/search/ui/TrackViewHolder.kt
+++ app/src/main/java/com/praktikum/playlistmaker/search/ui/TrackViewHolder.kt
@@ -2,7 +2,6 @@ package com.praktikum.playlistmaker.search.ui
 
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.praktikum.playlistmaker.R
 import com.praktikum.playlistmaker.databinding.ItemTrackBinding
 import com.praktikum.playlistmaker.search.data.model.Track
@@ -11,13 +10,13 @@ class TrackViewHolder(
     private val binding: ItemTrackBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(track: Track) {
-        val radiusPx = itemView.context.resources.getDimensionPixelSize(R.dimen.album_art_corner_radius)
         Glide
             .with(itemView.context)
             .load(track.artworkUrl100)
             .placeholder(R.drawable.album_placeholder)
             .error(R.drawable.album_placeholder)
-            .transform(RoundedCorners(radiusPx))
+            .centerCrop()
+            .fitCenter()
             .into(binding.trackAlbumArt)
 
         binding.trackTitle.text = track.trackName
