55547554dec3c0fcf5f14a1b635df43ebea5d7db
Xenia Dorofeeva <xenia.edu.projects@gmail.com>
2026-04-25 19:42:13 +0200

    Keyboard shouldn't overlay tracks viewgroup

--- app/src/main/java/com/praktikum/playlistmaker/search/ui/SearchActivity.kt
+++ app/src/main/java/com/praktikum/playlistmaker/search/ui/SearchActivity.kt
@@ -32,7 +32,13 @@ class SearchActivity : AppCompatActivity() {
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            val ime = insets.getInsets(WindowInsetsCompat.Type.ime())
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom + ime.bottom
+            )
             insets
         }
 
