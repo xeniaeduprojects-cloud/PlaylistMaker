0d0d3cf04af565930753b337686559c3194dd440
Xenia Dorofeeva <xenia.edu.projects@gmail.com>
2026-05-09 23:41:01 +0200

    support long values from the API case

--- app/src/androidTest/java/com/praktikum/playlistmaker/player/ui/PlayerUiTest.kt
+++ app/src/androidTest/java/com/praktikum/playlistmaker/player/ui/PlayerUiTest.kt
@@ -1,11 +1,14 @@
 package com.praktikum.playlistmaker.player.ui
 
+import android.text.Layout
 import android.view.View
+import android.widget.TextView
 import androidx.core.widget.NestedScrollView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
+import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -15,8 +18,10 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.praktikum.playlistmaker.R
 import com.praktikum.playlistmaker.search.data.model.Track
+import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.not
+import org.hamcrest.TypeSafeMatcher
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -208,6 +213,124 @@ class PlayerUiTest {
     }
 
     @Test
+    fun long_track_title_is_truncated_with_ellipsis() {
+        val track =
+            track(
+                name = "This Is An Extremely Long Track Name That Cannot Possibly Fit On Two Lines Of Text On A Normal Screen",
+            )
+
+        ActivityScenario.launch<PlayerActivity>(
+            PlayerActivity.createIntent(InstrumentationRegistry.getInstrumentation().targetContext, track),
+        )
+
+        onView(withId(R.id.tv_track_title)).check(matches(isEllipsized()))
+    }
+
+    @Test
+    fun long_artist_name_is_truncated_with_ellipsis() {
+        val track =
+            track(
+                artist = "Very Very Very Very Very Long Artist Name That Should Not Fit On A Single Line",
+            )
+
+        ActivityScenario.launch<PlayerActivity>(
+            PlayerActivity.createIntent(InstrumentationRegistry.getInstrumentation().targetContext, track),
+        )
+
+        onView(withId(R.id.tv_artist_name)).check(matches(isEllipsized()))
+    }
+
+    @Test
+    fun long_album_name_is_truncated_with_ellipsis() {
+        val track =
+            track(
+                collectionName = "An Exceptionally Long Album Title That Will Definitely Not Fit In The Available Space Next To The Label",
+            )
+
+        ActivityScenario.launch<PlayerActivity>(
+            PlayerActivity.createIntent(InstrumentationRegistry.getInstrumentation().targetContext, track),
+        )
+
+        onView(withId(R.id.tv_album)).perform(nestedScrollTo()).check(matches(isEllipsized()))
+    }
+
+    @Test
+    fun long_genre_is_truncated_with_ellipsis() {
+        val track =
+            track(
+                primaryGenreName = "Psychedelic Progressive Alternative Experimental Electronic Rock And Roll",
+            )
+
+        ActivityScenario.launch<PlayerActivity>(
+            PlayerActivity.createIntent(InstrumentationRegistry.getInstrumentation().targetContext, track),
+        )
+
+        onView(withId(R.id.tv_genre)).perform(nestedScrollTo()).check(matches(isEllipsized()))
+    }
+
+    @Test
+    fun long_country_is_truncated_with_ellipsis() {
+        val track =
+            track(
+                country = "The United Kingdom Of Great Britain And Northern Ireland And Some Other Territories",
+            )
+
+        ActivityScenario.launch<PlayerActivity>(
+            PlayerActivity.createIntent(InstrumentationRegistry.getInstrumentation().targetContext, track),
+        )
+
+        onView(withId(R.id.tv_country)).perform(nestedScrollTo()).check(matches(isEllipsized()))
+    }
+
+    @Test
+    fun long_album_does_not_overlap_label() {
+        val track =
+            track(
+                collectionName = "An Exceptionally Long Album Title That Will Definitely Not Fit In The Available Space Next To The Label",
+            )
+
+        ActivityScenario.launch<PlayerActivity>(
+            PlayerActivity.createIntent(InstrumentationRegistry.getInstrumentation().targetContext, track),
+        )
+
+        onView(withId(R.id.tv_album))
+            .perform(nestedScrollTo())
+            .check(doesNotOverlapWith(R.id.label_album))
+    }
+
+    @Test
+    fun long_genre_does_not_overlap_label() {
+        val track =
+            track(
+                primaryGenreName = "Psychedelic Progressive Alternative Experimental Electronic Rock And Roll",
+            )
+
+        ActivityScenario.launch<PlayerActivity>(
+            PlayerActivity.createIntent(InstrumentationRegistry.getInstrumentation().targetContext, track),
+        )
+
+        onView(withId(R.id.tv_genre))
+            .perform(nestedScrollTo())
+            .check(doesNotOverlapWith(R.id.label_genre))
+    }
+
+    @Test
+    fun long_country_does_not_overlap_label() {
+        val track =
+            track(
+                country = "The United Kingdom Of Great Britain And Northern Ireland And Some Other Territories",
+            )
+
+        ActivityScenario.launch<PlayerActivity>(
+            PlayerActivity.createIntent(InstrumentationRegistry.getInstrumentation().targetContext, track),
+        )
+
+        onView(withId(R.id.tv_country))
+            .perform(nestedScrollTo())
+            .check(doesNotOverlapWith(R.id.label_country))
+    }
+
+    @Test
     fun all_optional_rows_hidden_when_all_null() {
         val track =
             Track(
@@ -239,4 +362,59 @@ class PlayerUiTest {
         onView(withId(R.id.label_country)).check(matches(not(isDisplayed())))
         onView(withId(R.id.tv_country)).check(matches(not(isDisplayed())))
     }
+
+    private fun track(
+        name: String = "Track Name",
+        artist: String = "Artist Name",
+        collectionName: String? = "Album Name",
+        releaseDate: String? = "2020",
+        primaryGenreName: String? = "Pop",
+        country: String? = "USA",
+    ): Track =
+        Track(
+            trackId = 1L,
+            trackName = name,
+            artistName = artist,
+            trackTime = "03:00",
+            artworkUrl100 = "https://example.com/art.jpg",
+            collectionName = collectionName,
+            releaseDate = releaseDate,
+            primaryGenreName = primaryGenreName,
+            country = country,
+        )
+
+    private fun doesNotOverlapWith(labelId: Int): ViewAssertion =
+        ViewAssertion { valueView, noViewFoundException ->
+            if (noViewFoundException != null) throw noViewFoundException
+
+            val labelView = valueView.rootView.findViewById<View>(labelId)
+
+            val labelLoc = IntArray(2)
+            labelView.getLocationOnScreen(labelLoc)
+            val labelRight = labelLoc[0] + labelView.width
+
+            val valueLoc = IntArray(2)
+            valueView.getLocationOnScreen(valueLoc)
+            val valueLeft = valueLoc[0]
+
+            assert(valueLeft >= labelRight) {
+                "Value view left (${valueLeft}px) overlaps label right (${labelRight}px)"
+            }
+        }
+
+    private fun isEllipsized(): TypeSafeMatcher<View> =
+        object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("TextView text should be ellipsized")
+            }
+
+            override fun matchesSafely(view: View): Boolean {
+                if (view !is TextView) return false
+                val layout: Layout = view.layout ?: return false
+                if (layout.lineCount == 0) return false
+                return (0 until layout.lineCount).any { line ->
+                    layout.getEllipsisCount(line) > 0
+                }
+            }
+        }
 }
