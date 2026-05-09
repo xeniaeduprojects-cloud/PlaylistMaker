1e1d00823f1f907b128d308ef95fb2d8b9c48987
Xenia Dorofeeva <xenia.edu.projects@gmail.com>
2026-05-09 16:20:52 +0200

    Render image with a glide

--- app/src/main/java/com/praktikum/playlistmaker/player/ui/PlayerViewModel.kt
+++ app/src/main/java/com/praktikum/playlistmaker/player/ui/PlayerViewModel.kt
@@ -6,19 +6,19 @@ import androidx.lifecycle.ViewModel
 import com.praktikum.playlistmaker.search.data.model.Track
 
 class PlayerViewModel(
-    track: Track?,
+    track: Track,
 ) : ViewModel() {
     private val _uiState =
         MutableLiveData<PlayerUiState>(
             PlayerUiState.Content(
-                trackName = track?.trackName ?: "",
-                artistName = track?.artistName ?: "",
-                duration = track?.trackTime ?: "",
-                album = track?.collectionName,
-                year = track?.releaseDate,
-                genre = track?.primaryGenreName,
-                country = track?.country,
-                artworkUrl = track?.artworkUrl100 ?: "",
+                trackName = track.trackName,
+                artistName = track.artistName,
+                duration = track.trackTime,
+                album = track.collectionName,
+                year = track.releaseDate,
+                genre = track.primaryGenreName,
+                country = track.country,
+                artworkUrl = track.artworkUrl512,
             ),
         )
     val uiState: LiveData<PlayerUiState> = _uiState
