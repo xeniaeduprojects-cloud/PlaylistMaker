8182cdc7ca98002525e187893345df3cfbb6c83c
Xenia Dorofeeva <xenia.edu.projects@gmail.com>
2026-05-09 22:25:45 +0200

    Add tests for the navigation requirements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -148,6 +148,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.espresso.intents)
+    androidTestImplementation(libs.androidx.espresso.contrib)
     androidTestImplementation(libs.androidx.test.core)
     androidTestImplementation(libs.koin.test)
 }
